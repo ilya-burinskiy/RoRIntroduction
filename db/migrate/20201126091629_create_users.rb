00ab727e0f6e3e1b28433e962608a969b50ae265
Ilya <burinskiy992@gmail/com>
2020-11-27 17:41:05 +0300

    User model fix
    - Added email and password properties

--- db/migrate/20201126091629_create_users.rb
+++ db/migrate/20201126091629_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :first_name, null: false
       t.string :last_name, null: false
+      t.string :email, null: false
+      t.string :password, null: false
 
       t.timestamps
     end
