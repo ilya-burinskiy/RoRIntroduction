02c7805d40a3172d82dc883128f2725a7155ac94
Ilya <burinskiy992@gmail/com>
2020-11-28 21:50:28 +0300

    Test by_category class method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.by_category(pattern)
+    tests_categories = Test.joins('INNER JOIN categories ON tests.category_id = categories.id')
+    tests_categories.where('categories.name LIKE :pattern', pattern: "%#{pattern}%").order('tests.title DESC')
+  end
 end
