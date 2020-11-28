e821a253322a88b87922db523003efe22b68e4f6
Ilya <burinskiy992@gmail/com>
2020-11-28 21:49:58 +0300

    User tests_by_level instance method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  def tests_by_level(level)
+    tests_ids = TestsStartedByUser.where(user_id: id).pluck(:test_id)
+    Test.where(id: tests_ids).where(level: level)
+  end
 end
