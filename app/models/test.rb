75a785e32223608d041b6b2888b5c4e8d1fcf255
Ilya <burinskiy992@gmail/com>
2020-11-29 17:26:35 +0300

    Test by category method fix
    Returning array of test titles

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
   def self.by_category(pattern)
     Test.joins(
-      'INNER JOIN categories ON tests.category_id = categories.id'
-    ).where(categories: {name: pattern}).order(title: :desc)
+      'JOIN categories ON tests.category_id = categories.id'
+    ).where(categories: {name: pattern}).order(title: :desc).pluck(:title)
   end
 end
