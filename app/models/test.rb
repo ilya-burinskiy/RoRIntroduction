02e6e2a9df7f875ce4863774c6f230e2b8deabee
Ilya <burinskiy992@gmail/com>
2020-11-29 15:37:03 +0300

    WHERE query fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,7 @@
 class Test < ApplicationRecord
   def self.by_category(pattern)
-    tests_categories = Test.joins('INNER JOIN categories ON tests.category_id = categories.id')
-    tests_categories.where('categories.name LIKE :pattern', pattern: "%#{pattern}%").order('tests.title DESC')
+    Test.joins(
+      'INNER JOIN categories ON tests.category_id = categories.id'
+    ).where(categories: {name: pattern}).order(title: :desc)
   end
 end
