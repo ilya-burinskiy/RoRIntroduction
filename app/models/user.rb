1276ba19acf6856578c479be282181c0bd34baa5
Ilya <burinskiy992@gmail/com>
2020-11-29 17:28:46 +0300

    User started tests by level method
    Returning started tests of a given level for the user who called
    started_tests_by_level method, not all tests with this level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   def started_tests_by_level(level)
     Test.joins(
-      'INNER JOIN tests_started_by_users ON tests_started_by_users.test_id = tests.id'
-    ).where(level: level)
+      'JOIN tests_started_by_users ON tests_started_by_users.test_id = tests.id'
+    ).where(
+      'level = :level AND tests_started_by_users.user_id = :user_id',
+      level: level, user_id: self.id
+    )
   end
 end
