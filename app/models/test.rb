57a641a4d4beff122cea8e3cbac782db4cad6917
Ilya <burinskiy992@gmail/com>
2020-11-29 19:49:49 +0300

    Test model fix
    Renamed class method by_category to list_titles_by_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,5 @@
 class Test < ApplicationRecord
-  def self.by_category(pattern)
+  def self.list_titles_by_category(pattern)
     Test.joins(
       'JOIN categories ON tests.category_id = categories.id'
     ).where(categories: { name: pattern }).order(title: :desc).pluck(:title)
