d00ff8170103901a0586e20f00469c2c37ab70f4
Ilya <burinskiy992@gmail/com>
2020-11-30 20:01:25 +0300

    Removed unnecessary dependent: :destroy params

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :questions, dependent: :destroy
   has_many :tests_started_by_users, dependent: :destroy
-  has_many :passing_users, through: :tests_started_by_users, source: :user, dependent: :destroy
+  has_many :passing_users, through: :tests_started_by_users, source: :user
   belongs_to :author, class_name: "User", foreign_key: "user_id"
 
   def self.list_titles_by_category(pattern)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :tests_started_by_users, dependent: :destroy
-  has_many :started_tests, through: :tests_started_by_users, source: :test, dependent: :destroy
+  has_many :started_tests, through: :tests_started_by_users, source: :test
   has_many :created_tests, class_name: "Test", dependent: :destroy
 
   def started_tests_by_level(level)
