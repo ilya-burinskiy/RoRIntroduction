03075f9b74707dc217a92b56413def495db1609d
Ilya <burinskiy992@gmail/com>
2020-11-30 14:24:20 +0300

    Test and User one-to-many association.
    Test might have one author, but user can be author for many tests.

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,8 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
   has_many :tests_started_by_users
-  has_many :users, through: :tests_started_by_users
+  has_many :passing_users, through: :tests_started_by_users, source: :user
+  belongs_to :author, class_name: "User", foreign_key: "user_id"
 
   def self.list_titles_by_category(pattern)
     Test.joins(
