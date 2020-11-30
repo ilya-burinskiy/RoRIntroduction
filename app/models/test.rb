a6861e78b26e3ebe09a67e5aa3d9e2fbc3c54d0c
Ilya <burinskiy992@gmail/com>
2020-11-30 16:41:43 +0300

    Added dependent: :destroy param to has_many method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,8 @@
 class Test < ApplicationRecord
   belongs_to :category
-  has_many :questions
-  has_many :tests_started_by_users
-  has_many :passing_users, through: :tests_started_by_users, source: :user
+  has_many :questions, dependent: :destroy
+  has_many :tests_started_by_users, dependent: :destroy
+  has_many :passing_users, through: :tests_started_by_users, source: :user, dependent: :destroy
   belongs_to :author, class_name: "User", foreign_key: "user_id"
 
   def self.list_titles_by_category(pattern)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :tests_started_by_users
-  has_many :started_tests, through: :tests_started_by_users, source: :test
-  has_many :created_tests, class_name: "Test"
+  has_many :tests_started_by_users, dependent: :destroy
+  has_many :started_tests, through: :tests_started_by_users, source: :test, dependent: :destroy
+  has_many :created_tests, class_name: "Test", dependent: :destroy
 
   def started_tests_by_level(level)
     Test.joins(
