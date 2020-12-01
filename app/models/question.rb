5ef5861bb89d9fe59c03e70abefb0a0682faa504
Ilya <burinskiy992@gmail/com>
2020-12-01 19:04:30 +0300

    Answer count validation fix

--- app/models/question.rb
+++ app/models/question.rb
@@ -8,6 +8,8 @@ class Question < ApplicationRecord
   private
 
   def validate_answer_count
-    errors.add(:answer_count) if answers.count < 1 || answers.count > 4
+    if answers.count < 1 || answers.count > 4
+      errors.add(:base, message: 'Question can have from 1 to 4 answers') 
+    end
   end
 end
