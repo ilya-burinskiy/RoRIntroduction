689ea5046adc0679f6d2d3190f0d293f02c20292
Ilya <burinskiy992@gmail/com>
2020-12-01 17:49:48 +0300

    Firt name, last name, email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   has_many :started_tests, through: :tests_started_by_users, source: :test
   has_many :created_tests, class_name: "Test", dependent: :destroy
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, format: { with: /[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+/,
+                                              message: 'Invalid email format' }
+
   def started_tests_by_level(level)
     started_tests.by_level(level)
   end
