8df4134903d37292f7cf112bed8248b1db9c078c
Ilya <burinskiy992@gmail/com>
2020-12-01 23:10:18 +0300

    Question validation fix
    Question is validated when the number of
    answers is zero

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,7 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :title, presence: true
-  validate :validate_answer_count, on: :create
+  validate :validate_answer_count, if: :should_validate?
 
   private
 
@@ -11,5 +11,10 @@ class Question < ApplicationRecord
     unless (1..4).include?(answers.count)
       errors.add(:base, message: 'Question can have from 1 to 4 answers') 
     end
+  end
+
+  def should_validate?
+    return false if Answer.count.zero?
+    true
   end
 end
