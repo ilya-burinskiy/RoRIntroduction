5867ee6664f117438fc0f2529f7edf347681ffbe
Ilya <burinskiy992@gmail/com>
2020-12-01 15:57:35 +0300

    Using scope method in started_tests_by_level

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,6 @@ class User < ApplicationRecord
   has_many :created_tests, class_name: "Test", dependent: :destroy
 
   def started_tests_by_level(level)
-    Test.joins(
-      'JOIN tests_started_by_users ON tests_started_by_users.test_id = tests.id'
-    ).where(
-      'level = :level AND tests_started_by_users.user_id = :user_id',
-      level: level, user_id: id
-    )
+    started_tests.by_level(level)
   end
 end
