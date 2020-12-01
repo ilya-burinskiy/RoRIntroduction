0b8641a69805de7c1d02eb0dce4d865d5d7662f5
Ilya <burinskiy992@gmail/com>
2020-12-01 17:53:27 +0300

    Question answer count validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,13 @@
 class Question < ApplicationRecord
   belongs_to :test
   has_many :answers, dependent: :destroy
+
+  validates :title, presence: true
+  validate :validate_answer_count, on: :create
+
+  private
+
+  def validate_answer_count
+    errors.add(:answer_count) if answers.count < 1 || answers.count > 4
+  end
 end
