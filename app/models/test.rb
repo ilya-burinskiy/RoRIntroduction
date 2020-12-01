eaac83720f54de979cdd807462159d698400b754
Ilya <burinskiy992@gmail/com>
2020-12-01 17:52:49 +0300

    Test title and level validations

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,12 @@ class Test < ApplicationRecord
   has_many :passing_users, through: :tests_started_by_users, source: :user
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
+  validates :title, presence: true
+  validates :level, numericality: { only_integer: true,
+                                    greater_than_or_equal_to: 0 },
+                    presence: true
+  validates :level, uniqueness: { scope: :title }
+
   scope :by_level, ->(level) { where(level: level) }
   scope :easy, -> { by_level(0..1) }
   scope :medium, -> { by_level(2..4) }
