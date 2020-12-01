ba87364b2e05bef6bc4c62b56518d98687084d0b
Ilya <burinskiy992@gmail/com>
2020-12-01 15:54:45 +0300

    Replaced class method list_titles_by_category by scope

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,11 +3,16 @@ class Test < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :tests_started_by_users, dependent: :destroy
   has_many :passing_users, through: :tests_started_by_users, source: :user
-  belongs_to :author, class_name: "User", foreign_key: "user_id"
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
-  def self.list_titles_by_category(pattern)
-    Test.joins(
-      'JOIN categories ON tests.category_id = categories.id'
-    ).where(categories: { name: pattern }).order(title: :desc).pluck(:title)
-  end
+  scope :by_level, ->(level) { where(level: level) }
+  scope :easy, -> { by_level(0..1) }
+  scope :medium, -> { by_level(2..4) }
+  scope :difficult, -> { by_level(5..Float::INFINITY) }
+
+  scope :list_titles_by_category, lambda { |pattern|
+                                    Test.joins(:category).where(
+                                      categories: { name: pattern }
+                                    ).order(title: :desc).pluck(:title)
+                                  }
 end
