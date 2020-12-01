d2ecd54331e0b8e0b8c93a8319e4674b35f75968
Ilya <burinskiy992@gmail/com>
2020-12-01 21:07:04 +0300

    Adding answer to question validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,14 @@
 class Answer < ApplicationRecord
   belongs_to :question
   validates :body, presence: true
+  validate :validate_add_to_question
   scope :correct, -> { where(correct: true) }
+
+  private
+
+  def validate_add_to_question
+    unless (0..3).include?(question.answers.count)
+      errors.add(:base, message: "Too many answers for question #{question.title}")
+    end
+  end
 end
