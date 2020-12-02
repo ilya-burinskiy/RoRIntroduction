29478b5ac67403482ba582f1c7b70dfde193985d
Ilya <burinskiy992@gmail/com>
2020-12-02 12:04:05 +0300

    Question validation fix
    - Deleted validate_answer_count
    - Deleted should_validate?
    - Added body uniqueness

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,19 +2,5 @@ class Question < ApplicationRecord
   belongs_to :test
   has_many :answers, dependent: :destroy
 
-  validates :title, presence: true
-  validate :validate_answer_count, if: :should_validate?
-
-  private
-
-  def validate_answer_count
-    unless (1..4).include?(answers.count)
-      errors.add(:base, message: 'Question can have from 1 to 4 answers') 
-    end
-  end
-
-  def should_validate?
-    return false if Answer.count.zero?
-    true
-  end
+  validates :body, uniqueness: true, presence: true
 end
