ed2a578af38527aeb4c56aaccdfe60b9e45970f8
Ilya <burinskiy992@gmail/com>
2020-12-02 12:05:11 +0300

    Added answer body uniqueness validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :question
-  validates :body, presence: true
+  validates :body, uniqueness: true, presence: true
   validate :validate_add_to_question
   scope :correct, -> { where(correct: true) }
 
