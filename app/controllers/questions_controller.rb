88db918e200229f24a1f04e39cd806a3601e2915
Ilya <burinskiy992@gmail/com>
2020-12-04 17:29:17 +0300

    Question controller fix
    - Added find_question callback, for show and destroy methods
    - Question is created only if it is valid

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,27 +1,32 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: %i[index]
+  before_action :find_question, only: %i[destroy show]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
-  rescue_from ActiveRecord::RecordInvalid, with: :rescue_with_invalid_question_params
 
   def index
     render json: @test.questions
   end
 
   def show
-    render json: Question.find_by!(id: params[:id])
+    render json: @question
   end
 
   def new
   end
 
   def create
-    question = Question.create!(question_params)
-    render json: question.inspect
+    question = Question.new(question_params)
+    if question.valid?
+      question.save
+      redirect_to test_questions_path
+    else
+      render :new
+    end
   end
 
   def destroy
-    Question.find_by!(id: params[:id]).delete
+    @question.destroy!
   end
 
   private
@@ -31,14 +36,14 @@ class QuestionsController < ApplicationController
   end
 
   def find_test
-    @test = Test.find_by!(id: params[:test_id])
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
   def rescue_with_question_not_found
     render plain: 'Question not found'
-  end
-
-  def rescue_with_invalid_question_params
-    render plain: 'Invalid question parameters'
   end
 end
