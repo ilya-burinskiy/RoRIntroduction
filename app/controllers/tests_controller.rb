9c3e7a8ac0ab3066051ce889981d120b3f33aacc
Ilya <burinskiy992@gmail/com>
2020-12-07 12:24:21 +0300

    Test controller and views fix
    - Deleted all_tests callback
    - Added show view
    - Using test.question.count in test partial

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,19 +1,14 @@
 class TestsController < ApplicationController
-  before_action :all_tests, only: %i[index]
   before_action :find_test, only: %i[show]
 
   def index
+    @tests = Test.all
   end
 
   def show
-    redirect_to test_questions_path(@test)
   end
 
   private
-
-  def all_tests
-    @tests = Test.all
-  end
 
   def find_test
     @test = Test.find(params[:id])
