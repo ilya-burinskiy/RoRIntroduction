e27c0ba1e34bbd53d4a6cf248703e072a28a56b2
Ilya <burinskiy992@gmail/com>
2020-12-07 15:22:26 +0300

    Using interpolation

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -3,6 +3,6 @@ module QuestionsHelper
   MODS = {new: 'Create New', edit: 'Edit'}.freeze
 
   def question_header(test_title, mode)
-    MODS[mode] + ' ' + test_title + ' ' + 'Question'
+    "#{MODS[mode]} #{test_title} Question"
   end
 end
