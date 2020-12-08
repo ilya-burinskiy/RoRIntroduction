ba382e277505613c3a7f102d4e62c6d9a3defb88
Ilya <burinskiy992@gmail/com>
2020-12-08 15:58:23 +0300

    Test controller fix
    - Added find_tests and find_questions callbacks

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,7 @@
 class TestsController < ApplicationController
   before_action :find_test, only: %i[show]
+  before_action :find_tests, only: %i[index]
+  before_action :find_questions, only: %i[show]
 
   def index
   end
@@ -12,7 +14,14 @@ class TestsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:id])
+  end
+
+  def find_tests
     @tests = Test.all
+  end
+
+  def find_questions
+    @questions = @test.questions
   end
 
 end
