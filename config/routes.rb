d3735a0750648216bc25308cb14bb9ac825daef5
Ilya <burinskiy992@gmail/com>
2020-12-08 14:05:04 +0300

    Shallow true option in question routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :tests
-
   resources :tests do
-    resources :questions
+    resources :questions, shallow: true
   end
 end
