ac941feda9b39945e40672486af0f71421b9bbbb
Ilya <burinskiy992@gmail/com>
2020-12-08 20:12:52 +0300

    Removed unnecessary callbacks in test controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,9 +1,8 @@
 class TestsController < ApplicationController
   before_action :find_test, only: %i[show]
-  before_action :find_tests, only: %i[index]
-  before_action :find_questions, only: %i[show]
 
   def index
+    @tests = Test.all
   end
 
   def show
@@ -15,13 +14,4 @@ class TestsController < ApplicationController
   def find_test
     @test = Test.find(params[:id])
   end
-
-  def find_tests
-    @tests = Test.all
-  end
-
-  def find_questions
-    @questions = @test.questions
-  end
-
 end
