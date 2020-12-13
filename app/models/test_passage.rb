6b2430c05ef48480122ce025b064134e76f14c0c
Ilya <burinskiy992@gmail/com>
2020-12-13 15:45:11 +0300

    Test passage accept fix
    - It is taken into account that there are
      more than one answer options

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -13,7 +13,7 @@ class TestPassage < ApplicationRecord
 
   def accept!(answer_ids)
     if correct_answer?(answer_ids)
-      self.correct_questions += 1
+      self.correct_questions += answer_ids.count
     end
 
     self.current_question = next_question
