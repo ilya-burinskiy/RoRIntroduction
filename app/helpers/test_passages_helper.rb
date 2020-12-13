ce5913818b6176c733a2fbf9af0bf6ecbe7a3e01
Ilya <burinskiy992@gmail/com>
2020-12-13 13:33:31 +0300

    TestPassageHelper fix
    - Moved passed? to TestPassage

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,19 +1,11 @@
 module TestPassagesHelper
   def test_passage_color
-    return "green" if passed?
-    "red"
+    @test_passage.passed? ? "green" : "red"
   end
 
   def test_passage_message
     test_title = @test_passage.test.title
-    return "The #{test_title} Test was Passed" if passed?
+    return "The #{test_title} Test was Passed" if @test_passage.passed?
     "The #{test_title} Test was not Passed"
-  end
-
-  private
-
-  def passed?
-    return true if @test_passage.passage_percent >= TestPassage::THRESHOLD
-    false
   end
 end
