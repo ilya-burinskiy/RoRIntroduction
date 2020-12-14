ebaa97f3f7e5e951f5bb77a69d957935a8398f06
Ilya <burinskiy992@gmail/com>
2020-12-14 16:59:58 +0300

    User authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
   has_many :test_passages, dependent: :destroy
   has_many :started_tests, through: :test_passages, source: :test
@@ -5,9 +7,8 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, uniqueness: true, presence: true,
-                    format: { with: URI::MailTo::EMAIL_REGEXP,
-                              message: 'Invalid email format' }
+
+  has_secure_password
 
   def started_tests_by_level(level)
     started_tests.by_level(level)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'users/new'
+
   root to: 'tests#index'
+
+  get :signup, to: 'users#new'
+
+  resources :users, only: :create
   
   resources :tests do
     resources :questions, shallow: true do
