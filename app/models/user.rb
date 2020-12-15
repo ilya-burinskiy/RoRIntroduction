0bcd44ce1b9cec25da8c36e964eb87fde2d31433
Ilya <burinskiy992@gmail/com>
2020-12-15 21:06:32 +0300

    User email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,10 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-
+  validates :email, uniqueness: true, presence: true,
+                    format: { with: URI::MailTo::EMAIL_REGEXP,
+                              message: 'Invalid email format' }
+                              
   has_secure_password
 
   def started_tests_by_level(level)
