5764cfdf2faff2d33f083d4abb8f3c469b533f70
Ilya <burinskiy992@gmail/com>
2020-12-19 14:26:49 +0300

    ApplicationController#after_sign_in_path_for fix
    - Added authenticate_user! callback
    - super call in after_sign_in_path_for if current_user is not admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(user)
-    user.admin? ? admin_tests_path : tests_path
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    current_user.admin? ? admin_tests_path : super
   end
 end
