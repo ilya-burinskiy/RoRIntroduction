7c0cfc640af667d820f95c6e7555ba19fa7949f9
Ilya <burinskiy992@gmail/com>
2020-12-22 21:12:17 +0300

    Gist model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_18_103426) do
+ActiveRecord::Schema.define(version: 2020_12_21_172916) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "question_id", null: false
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 2020_12_18_103426) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "gists", force: :cascade do |t|
+    t.integer "question_id", null: false
+    t.string "url", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_gists_on_question_id"
+    t.index ["user_id"], name: "index_gists_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -88,6 +98,8 @@ ActiveRecord::Schema.define(version: 2020_12_18_103426) do
   end
 
   add_foreign_key "answers", "questions"
+  add_foreign_key "gists", "questions"
+  add_foreign_key "gists", "users"
   add_foreign_key "questions", "tests"
   add_foreign_key "test_passages", "questions"
   add_foreign_key "test_passages", "tests"
