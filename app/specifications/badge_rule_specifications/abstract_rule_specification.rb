637906603e4efb40de4b12ac1abaa6b62c6bed4c
Ilya <burinskiy992@gmail/com>
2021-01-28 19:04:12 +0300

    Added AbstractRuleSpecification to BadgeSpecifications module

--- app/specifications/badge_rule_specifications/abstract_rule_specification.rb
+++ app/specifications/badge_rule_specifications/abstract_rule_specification.rb
@@ -1,10 +1,12 @@
-class AbstractRuleSpecification
-  def initialize(test_passage, rule_property)
-    @test_passage = test_passage
-    @rule_property = rule_property
-  end
-  
-  def satisfies?
-    raise "#{__method__} undefined for #{self.class}"
+module BadgeRuleSpecifications
+  class AbstractRuleSpecification
+    def initialize(test_passage, rule_property)
+      @test_passage = test_passage
+      @rule_property = rule_property
+    end
+    
+    def satisfies?
+      raise "#{__method__} undefined for #{self.class}"
+    end
   end
 end
