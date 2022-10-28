84ee3393afdfdb8ce66553b1aae4016c26d9fad2
Ilya Burinsky <burinskiy992@gmail.com>
2022-10-28 21:04:03 +0300

    dep: confiqured app for rspec gem

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -46,4 +46,7 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+
+  # For separating factories from sequences
+  config.factory_bot.definition_file_paths << 'spec/sequences'
 end
